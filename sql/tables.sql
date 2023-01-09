59d10734bbd54c8314ab4cb0cf7cd7c1388a807f
chramb <79099832+chramb@users.noreply.github.com>
2023-01-09 21:58:39 +0100

    removed tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,7 +1,11 @@
 
+create table "cpu" (
+                       "cpu_id" serial primary key,
+                       "name" varchar(128) unique
+);
 create table "brand" (
                          "brand_id" serial primary key,
-                         "name" varchar(16)
+                         "name" varchar(16) unique
 );
 create table "user"
 (
@@ -35,24 +39,22 @@ create table "phone"
     "thickness"         float4,
     "resolution"        varchar(12), -- 1024x720
     "ppi"               int,
-    "color_id"          varchar(32),
+    "cpu_id"            int references cpu(cpu_id),
     "chipset_id"        int references chipset (chipset_id),
     "gpu_id"            int references gpu (gpu_id),
-    "memory_card_max_gb"   int,
     "memory_card_dedicated" bool,
-    "internal memory"   int4[],
+    "internal_memory"   int4[],
     "ram"               int4[],
-    "wifi"              varchar(16)[],
-
+    "wifi"              varchar(32),
+    "sim"               varchar(128),
     "connector"         varchar(32),
-    "loud_speakers"     bool,
     "audio_jack"        bool,
     "bluetooth_version" float4,
     "gps"               bool,
     "nfc"               bool,
     "radio"             bool,
     "battery_capacity"  int,
-    "battery_removable" int,
+    "battery_removable" bool,
     "image_url"         varchar(128) -- check length of urls
 );
 
@@ -85,46 +87,10 @@ create table "camera" (
     "camera_id" serial primary key,
     "mp" int4,
     "f" float4,
-    "flash" bool,
-    "resolution" int
-);
-create table "cpu" (
-  "cpu_id" serial primary key,
-  "name" varchar(12),
-  "frequency" float4,
-  unique ("frequency", "name")
-);
-create table "color" (
-  "color_id" serial primary key,
-  "name" varchar(64)
-);
-create table "sim" (
-                       "sim_id" serial primary key,
-                       "type" varchar(16)
-);
-
-create table "network" (
-    "network_id" serial primary key,
-    "symbol" varchar(16)-- 2G 4G etc
+    unique (mp, f)
 );
 
 
-create table "phone-cpu" (
-    -- 1 phone multiple cpus (each core separate cpu)
-                             "phone_id" int references phone(phone_id) on delete cascade,
-                             "cpu_id" int references cpu(cpu_id) on delete cascade,
-                             primary key (phone_id, cpu_id)
-);
-create table "phone-network" (
-                                 "phone_id" int references phone(phone_id) on delete cascade,
-                                 "network_id" int references network(network_id) on delete cascade,
-                                 primary key (phone_id, network_id)
-);
-create table "phone-sim" (
-    phone_id int references phone(phone_id) on delete cascade,
-    sim_id int references sim(sim_id) on delete cascade,
-    primary key (phone_id, sim_id)
-);
 create table "phone-camera" (
                                             phone_id int references phone(phone_id) on delete cascade,
                                             camera_id int references camera(camera_id) on delete cascade,
