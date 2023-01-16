3b2c323f3c5c325420d2ea1b2072992d410cae80
chramb <79099832+chramb@users.noreply.github.com>
2023-01-16 19:34:38 +0100

    added config support to db_container.py script

--- scripts/db_container.py
+++ scripts/db_container.py
@@ -2,18 +2,20 @@
 
 from config import config
 from subprocess import run
+from pathlib import PurePath
+
 cfg = config(section="postgres")
 
 cmd = f"""
 podman run -d \\
-       --name forum-db \\
+       --name TelefonyDB-1 \\
         -e POSTGRES_USER={cfg['user']} \\
         -e POSTGRES_PASSWORD={cfg['password']} \\
         -e POSTGRES_DB={cfg['database']} \\
         -p {cfg['port']}:5432 \\
-        -v $PWD/pgdata:/var/lib/postgresql/data:rw,Z \\
+        -v {PurePath(__file__).parents[1]}/pgdata:/var/lib/postgresql/data:rw,Z \\
     postgres:12-alpine
 """
 print(cmd)
 
-# run(['sh', '-c', cmd])
+run(['sh', '-c', cmd])
